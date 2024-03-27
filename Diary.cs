f5f8b02fc21ebf77a7e99d46096046e089fe458b
duyga16 <131605874+duyga16@users.noreply.github.com>
2024-03-27 22:10:59 +0700

    Add files via upload

---
+++ Diary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CNPM.Model;
+using System.Data;
+using MySql.Data.MySqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace CNPM.Controller
+{
+    internal class Diary
+    {
+        private string connectionString = "Server=localhost;Port=3306;Database=cnpm_n11;Uid=root;Pwd=;charset=utf8";
+        public DataTable GetCombinedData()
+        {
+            DataTable dt = new DataTable();
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT P.productID, P.name, P.price, O.orderDate, O.orderDate" +
+                               " FROM Products P INNER JOIN Orders O ON O.orderID = P.orderID;";
+                MySqlCommand command = new MySqlCommand(query, connection);
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))    
+                {
+                    adapter.Fill(dt);
+                }
+            }
+            return dt;
+        }
+    }
+}
