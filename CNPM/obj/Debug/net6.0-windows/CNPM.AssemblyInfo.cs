3e77089eace79e1637cf5531f0ebe9a25b8720b3
quigiaosu103 <52100996@student.tdtu.edu.vn>
2024-03-28 13:31:46 +0700

    cm

--- CNPM/obj/Debug/net6.0-windows/CNPM.AssemblyInfo.cs
+++ CNPM/obj/Debug/net6.0-windows/CNPM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CNPM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce89485e1194a611f26fbbda91c3047b5d864005")]
 [assembly: System.Reflection.AssemblyProductAttribute("CNPM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CNPM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
