b7888d37b96a6f8d02c519a468d5e28169d18af3
quigiaosu103 <52100996@student.tdtu.edu.vn>
2024-03-28 12:52:02 +0700

    merge branch tkhoi

--- CNPM/obj/Debug/net6.0-windows/CNPM.AssemblyInfo.cs
+++ CNPM/obj/Debug/net6.0-windows/CNPM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CNPM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+749f8504cfd77d11526e27996136733fa3165f1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CNPM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CNPM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
