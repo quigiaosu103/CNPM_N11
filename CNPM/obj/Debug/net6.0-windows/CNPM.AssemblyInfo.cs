790837226a95c10d5731bf66bbbb9390481aaa99
codeduy <nguyentranduy19@gmail.com>
2024-05-20 14:22:26 +0700

    cake_shop

--- CNPM/obj/Debug/net6.0-windows/CNPM.AssemblyInfo.cs
+++ CNPM/obj/Debug/net6.0-windows/CNPM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CNPM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffb1d7d1ee1826239367e6db4a89045b406f3c28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+188bb3932724c3fa12c76e995f996eda59003154")]
 [assembly: System.Reflection.AssemblyProductAttribute("CNPM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CNPM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
